00f9e3566c4601f9fd2d5f0e5c927b0fd1f7ee93
ashwinivader <ashwinivader@outlook.com>
2025-04-01 20:47:10 +0530

    chnage in yml2

--- browserlaunch.py
+++ browserlaunch.py
@@ -1,19 +1,32 @@
+import tempfile
+import shutil
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service as ChromeService
+#from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
+import time
 
 # Set up the Chrome WebDriver using WebDriver Manager
 #driver = webdriver.Chrome()
+
+# Create a temporary directory for the user data
+
+
+
 options = ChromeOptions()
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 # Open Google
 driver.get("https://dev-www.astm.org/")
 
 # Optional: Wait for a few seconds to see the browser (not required, just for demo purposes)
-import time
-time.sleep(5)
+
+time.sleep(2)
+
+
 
 # Close the browser
 driver.quit()
