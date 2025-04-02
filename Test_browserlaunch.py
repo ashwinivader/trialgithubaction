dd035eca7e51d32dcda3b7c1b6ea9f3a4b2a66bc
ashwinivader <ashwinivader@outlook.com>
2025-04-02 09:31:37 +0530

    added

--- Test_browserlaunch.py
+++ Test_browserlaunch.py
@@ -8,12 +8,18 @@ from selenium.webdriver.chrome.service import Service
 import time
 import pytest
 from selenium.webdriver.chrome.options import Options
+import logging
 
 # Set up the Chrome WebDriver using WebDriver Manager
 #driver = webdriver.Chrome()
 
 # Create a temporary directory for the user data
-
+# Set up logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 def testlaunch():
         print("Trial")
@@ -28,12 +34,13 @@ def testlaunch():
 
 
         options = Options()
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=options)
         driver.get("https://dev-www.astm.org/")
         time.sleep(1)
         print('Page Title:', driver.title)
+        logger.info('Page Title:', driver.title)
         assert 'Home | ASTM' in driver.title
         driver.quit()
